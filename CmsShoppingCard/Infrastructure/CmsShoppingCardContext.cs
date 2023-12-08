223fb9cf9fcfb1c47fe78ada80c2e91406961016
medLmtToInfinity <med.lmt012@gmail.com>
2023-12-08 16:22:42 +0100

    Creating DbContext

---
+++ CmsShoppingCard/Infrastructure/CmsShoppingCardContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCard.Infrastructure
+{
+    public class CmsShoppingCardContext: DbContext
+    {
+        public CmsShoppingCardContext(DbContextOptions<CmsShoppingCardContext> options): base(options)
+        {
+
+        }
+        {
+            
+        }
+    }
+}
